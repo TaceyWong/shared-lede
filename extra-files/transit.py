1fef4a704ce93ca7a5eafa2df59239e89b356e55
王新勇(Tacey Wong) <xinyong.w@gmail.com>
2025-04-29 00:47:47 +0800

    Update transit.py

--- extra-files/transit.py
+++ extra-files/transit.py
@@ -92,11 +92,11 @@ def generate_temp_modify_sh(temp, ori, config, ip, pwd):
             new = 'lan) ipad=${ipaddr:-"' + ip + '"} ;;'
             f.write(
                 f"\nsed -i '/lan) ipad=/c{new}' package/base-files/files/bin/config_generate\n")
-        f.write(
-                f"\nsed -i 'ImmortalWrt/cicosmos.space' package/base-files/files/bin/config_generate\n")
         if pwd:
             f.write(
                 f"\nsed -i '/root/c{crypt_str(pwd)}' package/base-files/files/etc/shadow\n")
+        f.write(
+                f"\nsed -i 's/ImmortalWrt/icosmos.space/g' package/base-files/files/bin/config_generate\n")
     return tm1
 
 
